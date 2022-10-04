09b95a078345da48d1909ad6764de0c10626d1fd
SATISH MAURYA <satishchandra-maurya>
2022-10-03 19:16:04 -0700

    09-Greet API pushing

---
+++ src/main/java/in/satish/rest/GreetRestController.java
@@ -0,0 +1,14 @@
+package in.satish.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetRestController {
+
+	@GetMapping("/greet")
+	public String greetApi() {
+		String msg = "Welcome to Greet Api";
+		return msg;
+	}
+}
